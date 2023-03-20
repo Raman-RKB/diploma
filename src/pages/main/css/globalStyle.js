a91a0bd3e39f423ac9d7626d67e755ed9c788938
Raman-RKB <121190700+Raman-RKB@users.noreply.github.com>
2023-03-21 02:51:39 +0300

    init commit

--- src/pages/main/css/globalStyle.js
+++ src/pages/main/css/globalStyle.js
@@ -2,48 +2,59 @@ import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-* {
+  * {
   margin: 0;
   padding: 0;
   -webkit-box-sizing: border-box;
           box-sizing: border-box;
-}
+  }
 
-*:before,
-*:after {
+  *:before,
+  *:after {
   -webkit-box-sizing: border-box;
           box-sizing: border-box;
-}
+  }
 
-a,
-a:visited {
+  a,
+  a:visited {
   text-decoration: none;
   font-family: 'StratosSkyeng', sans-serif;
   cursor: pointer;
-}
+  }
 
-button,
-._btn {
+  button,
+  ._btn {
   cursor: pointer;
-}
+  }
 
-ul li {
+  ul li {
   list-style: none;
-}
+  }
 
-html,
-body {
+  html,
+  body {
   width: 100%;
   height: 100%;
   font-family: 'Roboto', sans-serif;
   color: #000000;
-}
+  }
 
-div,
-button,
-a {
+  div,
+  button,
+  a {
   font-family: 'Roboto', sans-serif;
-}
+  }
+
+    // ========== fonts ========
+  @font-face {
+    font-family: 'Roboto';
+    src: local('Roboto'), local('Roboto'),
+        url('../fonts/Roboto-Bold.ttf') format('ttf'),
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  //--------------------------
 
 `;
 
